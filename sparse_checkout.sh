1940a12503c94b846feceea68e9e0e6ae9e29c82
mcr1321 <mcr1321@users.noreply.github.com>
2023-02-11 19:09:27 -0800

    Update sparse_checkout.sh

--- sparse_checkout.sh
+++ sparse_checkout.sh
@@ -1,12 +1,11 @@
-infrastructure_version=terraform   #options: terraform / bicep 
-project_type=classical      #options: classical / cv / nlp
+infrastructure_version=bicep   #options: terraform / bicep 
+project_type=classical  # classical
 mlops_version=aml-cli-v2   #options: aml-cli-v2 / python-sdk-v1 / python-sdk-v2 / rai-aml-cli-v2
-orchestration=azure-devops #options: github-actions / azure-devops
-git_folder_location='<local path>'   #replace with the local root folder location where you want to create the project folder
-project_name=Mlops-Test   #replace with your project name
-github_org_name=orgname   #replace with your github org name
-project_template_github_url=https://github.com/azure/mlops-project-template   #replace with the url for the project template for your organization created in step 2.2, or leave for demo purposes
-
+git_folder_location='C:\Users\mcadenaroa\source\repos\mlopsv2root'   #replace with the local root folder location where you want to create the project folder
+project_name=mlopsv2-sparse   #replace with your project name
+github_org_name=mcr1321   #replace with your github org name
+project_template_github_url=https://github.com/mcr1321/mlops-v2   #replace with the url for the project template for your organization created in step 2.2, or leave for demo purposes
+orchestration=github-actions #options: github-actions / azure-devops
 cd $git_folder_location
 
 # Clone MLOps Project repo
